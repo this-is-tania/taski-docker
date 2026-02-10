4387e533fb1544a41d830a901c095e3810c181db
Tatiana <tmalakh@yahoo.com>
2026-02-10 22:14:43 +0300

    Updated Allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,9 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv(
-    'ALLOWED_HOSTS',
-    'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '89.169.180.140', 'thisistania.hopto.org']
 
 
 # Application definition
